d474ac4e6064319808472516fce0cd3faca74d69
mounikavinoth <mounikavanitha22@gmail.com>
2016-08-16 18:22:04 +0530

    Add files via upload

---
+++ zero.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class zero
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number:");
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.println("positive");
+}
+else if(n<0)
+{
+System.out.println("negative");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
+
