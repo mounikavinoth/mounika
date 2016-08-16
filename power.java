ec721b9b8beb1adad346d58d50cf82311eccf11d
mounikavinoth <mounikavanitha22@gmail.com>
2016-08-16 20:58:27 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class power
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the numbers:");
+int n=sc.nextInt();
+int n1=sc.nextInt();
+double j=Math.pow(n,n1);
+System.out.println(j);
+}
+}
